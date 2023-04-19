31074851c0ed65ecc4ac527972338a3477b1e646
HectorRamirez <eduardo.846@gmail.com>
2023-04-19 12:44:40 -0400

    Mostrar Mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,12 @@
-alert('Soy el mapa')
+(function() {
+
+    const lat = 7.9197131;
+    const lng = -72.4981874
+    const mapa = L.map('mapa').setView([lat, lng ], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
