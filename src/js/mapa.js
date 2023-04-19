785db3610a9824d6bb61f7b7f0664eb4650b0b0e
HectorRamirez <eduardo.846@gmail.com>
2023-04-19 14:40:58 -0400

    Añidiendo un Pin con la ubicacion de la propiedad

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,10 +3,20 @@
     const lat = 7.9197131;
     const lng = -72.4981874
     const mapa = L.map('mapa').setView([lat, lng ], 13);
+    let marker;
+
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    //El pin
+    marker =  new L.marker([lat, lng],{
+        draggable: true,
+        autoPan: true
+    })
+    .addTo(mapa)
+
 
 
 })()
