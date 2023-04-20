250ea7d63c7f1c962a15740f6720c64734e6dc34
HectorRamirez <eduardo.846@gmail.com>
2023-04-20 12:04:10 -0400

    Obtener Informacion de la Calle y Ciudad al soltar el Pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -4,6 +4,10 @@
   const mapa = L.map("mapa").setView([lat, lng], 13);
   let marker;
 
+  //Utilizar Provider y Geocoder
+
+    const geocodeService = L.esri.Geocoding.geocodeService();
+
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -19,5 +23,11 @@
     marker = e.target;
     const posicion = marker.getLatLng();
     mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
+    //Obtener la informacion de las calles al soltar el pin
+
+    geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado){
+        //console.log(resultado)
+        marker.bindPopup(resultado.address.LongLabel)
+    })
   });
 })();
