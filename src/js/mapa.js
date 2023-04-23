8bc851f731312087713ad2a9e230488a46d59ce7
HectorRamirez <eduardo.846@gmail.com>
2023-04-23 09:51:23 -0400

    Almacenando latitud, Longitud y calle

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -28,6 +28,12 @@
     geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado){
         //console.log(resultado)
         marker.bindPopup(resultado.address.LongLabel)
+
+        // Llenar los campos
+        document.querySelector('.calle').textContent = resultado?.address?.Address ??  '';
+        document.querySelector('#calle').value = resultado?.address?.Address ??  '';
+        document.querySelector('#lat').value = resultado?.latlng?.lat ??  '';
+        document.querySelector('#lng').value = resultado?.latlng?.lng ??  '';
     })
   });
 })();
