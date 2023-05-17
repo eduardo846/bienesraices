fa67e1b97287f769fda5863316e810ef225a86be
HectorRamirez <eduardo.846@gmail.com>
2023-05-17 17:07:40 -0400

    Añidiendo el Resto de validaciones

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -8,6 +8,15 @@ router.get("/propiedades/crear", crear);
 router.post(
   "/propiedades/crear",
   body("titulo").notEmpty().withMessage("El titulo del Anuncio es Obligatorio"),
+  body("descripcion")
+        .notEmpty().withMessage("La descripcion no puede ir vacia")
+        .isLength({max:200}).withMessage('La descripcion es muy larga'),
+  body('categoria').isNumeric().withMessage('Selecciona una categoria'),
+  body('precio').isNumeric().withMessage('Selecciona un rango de precio'),
+  body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+  body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+  body('wc').isNumeric().withMessage('Selecciona la cantidad de baños'),
+  body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
   guardar
 );
 
