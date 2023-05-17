af767d14498824db870c404684eb89d174caa628
HectorRamirez <eduardo.846@gmail.com>
2023-05-17 11:49:46 -0400

    Importando precios y categoriaspara pasarlos a la vista de crear

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,5 @@
+import Precio from "../models/Precio.js";
+import Categoria from "../models/Categoria.js";
 const admin = (req, res) => {
   res.render("propiedades/admin", {
     pagina: "Mis propiedades",
@@ -6,10 +8,17 @@ const admin = (req, res) => {
 };
 
 //! Formulario para crear una propiedad
-const crear = (req, res) => {
+const crear = async (req, res) => {
+  //Consultar modelo de precio y  categorias
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll(),
+  ]);
   res.render("propiedades/crear", {
     pagina: "Crear Propiedad",
     barra: true,
+    categorias,
+    precios,
   });
 };
 
