7a8f340824f43f569bafc14b8473d5340d295815
HectorRamirez <eduardo.846@gmail.com>
2023-05-22 12:07:40 -0400

    Leyendo y Validando el JWT

--- middleware/protegerRuta.js
+++ middleware/protegerRuta.js
@@ -1,5 +1,19 @@
+import jwt from 'jsonwebtoken'
+import {Usuario} from '../models/index.js'
 const protegerRuta = async (req, res, next) => {
-  console.log("Desde el Middleware");
+  //Verificar si hay un token
+  const {_token} = req.cookies
+  if(!_token){
+    return res.redirect('/auth/login')
+  }
+  //Comprobar el token
+  try {
+    const decoded = jwt.verify(_token, process.env.JWT_SECRET)
+    const usuario = await Usuario.findByPk(decoded.id)
+    console.log(usuario)
+  } catch (error) {
+    return res.clearCookies('_token').redirect('/auth/login')
+  }
   next();
 };
 export default protegerRuta;
